39d4e718369d6e14f2778b6ce9be3a7ddbb00d6a
raissa-placido <raissaplacido0@gmail.com>
2026-08-31 07:28:59 -0300

    fix: modificando o teste para utilizar o vitest na questão 01.

--- src/ex01/testes/ex01.test.ts
+++ src/ex01/testes/ex01.test.ts
@@ -1,14 +1,12 @@
-import test from 'node:test';
-import assert from 'node:assert/strict';
-import { elevarAoQuadradoComFor, elevarAoQuadradoComForEach } from '../ex01.js';
+import { test, expect } from 'vitest';
+import { elevarAoQuadradoComFor, elevarAoQuadradoComForEach } from '../ex01';
 
 const input = [3, 5, 7, 3, 8, 9, 1];
 const expected = [9, 25, 49, 9, 64, 81, 1];
 
 test('1.1 Deve elevar ao quadrado usando "for"', () => {
-  assert.deepStrictEqual(elevarAoQuadradoComFor(input), expected);
-});
+  expect(elevarAoQuadradoComFor(input)).toEqual(expected);});
 
 test('1.2 Deve elevar ao quadrado usando "forEach"', () => {
-  assert.deepStrictEqual(elevarAoQuadradoComForEach(input), expected);
+    expect(elevarAoQuadradoComForEach(input)).toEqual(expected);
 });
